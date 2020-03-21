35bec9f7f60cb3f5ce8431cc67df3f0787a75e28
SK <stephanie@sfdigitalstudio.com>
2020-03-21 14:57:59 -0700

    updated js file with showing high scores

--- README.md
+++ README.md
@@ -11,10 +11,12 @@ This application runs a short 3 question quiz on development languages.
 ## How does it work?
 The application is a three question multiple choice quiz. In this quiz, the user will have to choose the correct answer out of three choices within 30 seconds. If the user answers the question correclty, the next question will appear.  If the user doesn't answer the question correctly, a 10 second time deduction penalty will happen. A high score table can be saved and viewed.
 
+Open the index.html in a browser to start the quiz
+
 ## Technologies
 
 ## Languages, frameworks, various tools
-This application uses Javascript, CSS, HTML, Bootstrap
+This application uses Javascript, JQuery, CSS, HTML, Bootstrap
 
 ## Challenges
 This assignment was challenging all around.  Functions are crazy and trying to tie all that we have learned together is a big task.
@@ -24,18 +26,20 @@ This app took me a while to complete as it is hard to work on things piecemeal. 
 
 ## What was difficult
 Trying to remember all the right syntax.
+Remember: to put the JQuery script tag in the HTML file before the .js file script tag
+
+
 
 ## What did you learn
 I learned more about functions and localStorage
 
 ## How did you go about solving a problem
-Many, times I had to Google items to further my knowledge in a topic.  Thank ngoodness for all the people who like to post things online.
+Many, times I had to Google items to further my knowledge in a topic.  Thank goodness for all the people who like to post things online.
 
 ## Screenshot
 
-I tried to paste a screenshot here but it wouldn't paste so let me know how to do that.  Thanks.
 
 
 ## Live app, not code
 
-Not sure what goes here.  Let me know.  Thanks.
+
--- assets/quiz.js
+++ assets/quiz.js
@@ -7,11 +7,19 @@ const quiz = document.getElementById('quiz');
 const message = document.getElementById('message');
 const name = document.getElementById('name');
 
-// pulls highscores from local storage.  If first time and nothing in localstorage, then make it an empty array
-  // local storage stores items as a string
-  // if want to store array, then need to use JSON.stringify to make a string
-  // to convert to an array, then need to use JSON.parse
-const highScores = JSON.parse(localStorage.getItem('highScore')) || [];
+// pulls highscores from local storage. 
+// local storage stores items as a string
+// if want to store array, then need to use JSON.stringify to make a string
+// to convert to an array, then need to use JSON.parse
+// create variable to store the high scores
+var scores = []
+var scores = JSON.parse(localStorage.getItem("highScore"));
+
+// If an array does not exist, then create a blank one.
+if (!scores) {
+  scores = []
+}
+
 
 let secondsLeft = 30;
 let currentQuestion = 0;
@@ -21,19 +29,19 @@ let score = 0;
 // questions for quiz
 const questions = [
   {
-      subject : "What does HTML stand for?",
-      choices : ["Hyper Text Markup Language", "How To Manipulate Language", "Hello To My Lady"],
-      answer : "Hyper Text Markup Language"
+    subject: "What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "How To Manipulate Language", "Hello To My Lady"],
+    answer: "Hyper Text Markup Language"
   },
   {
-      subject : "What does CSS stand for?",
-      choices : ["Cascading Style Sets", "Cascading Style Sheets", "Coding Source Sheets"],
-      answer : "Cascading Style Sheets"
+    subject: "What does CSS stand for?",
+    choices: ["Cascading Style Sets", "Cascading Style Sheets", "Coding Source Sheets"],
+    answer: "Cascading Style Sheets"
   },
   {
-      subject : "What does JS stand for?",
-      choices : ["JavaSit", "JavaSplit", "JavaScript"],
-      answer : "JavaScript"
+    subject: "What does JS stand for?",
+    choices: ["JavaSit", "JavaSplit", "JavaScript"],
+    answer: "JavaScript"
   }
 ];
 
@@ -46,34 +54,35 @@ function startQuiz() {
   timerCountdown();
   quiz.innerText = 'Ready?'
   // waits 1 second to display the question so the question and timer show at the same time
-  setTimeout (() => {
+  setTimeout(() => {
     renderQuestion();
-  },1000);
+  }, 1000);
 };
 
 // Starts timer
 // when the timer hits zero then clear the timer and end the game
 function timerCountdown() {
-	var timerInterval = setInterval(function () {
-		secondsLeft--;
-		timer.innerHTML = `
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timer.innerHTML = `
 		<div>Time Remaining: ${secondsLeft} seconds
 		<br>
     </div>
 		`;
-		if (secondsLeft <= 0) {
-			clearInterval(timerInterval);
-			return endGame();
-		}
-	}, 1000);
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      return endGame();
+    }
+  }, 1000);
 };
+
 
 // Generates questions and answer option buttons
 function renderQuestion() {
-	let questList = questions[currentQuestion].choices.map((question) => {
-		return `<button class="answerBtn" onclick="checkAnswer('${question}')">${question}</button>`;
-	});
-	quiz.innerHTML = `${questions[currentQuestion].subject}<br>${questList.join("")}`;
+  let questList = questions[currentQuestion].choices.map((question) => {
+    return `<button class="answerBtn" onclick="checkAnswer('${question}')">${question}</button>`;
+  });
+  quiz.innerHTML = `${questions[currentQuestion].subject}<br>${questList.join("")}`;
 };
 
 // Adds to score if answered correctly
@@ -81,54 +90,71 @@ function renderQuestion() {
 // Shows message about answer
 // When all questions are answered, then quiz ends
 function checkAnswer(selection) {
-	if (questions[currentQuestion].answer === selection) {
+  if (questions[currentQuestion].answer === selection) {
     message.innerHTML = '<p> Correct</p>';
-		score++;
-	} else {
+    score++;
+  } else {
     message.innerHTML = '<p> Wrong</p>';
     secondsLeft -= 10;
-	}
-	currentQuestion++;
-	if (currentQuestion === questions.length) {
-		return endGame();
-	}
-	renderQuestion();
+  }
+  currentQuestion++;
+  if (currentQuestion === questions.length) {
+    return endGame();
+  }
+  renderQuestion();
 };
 
 
 // Displays your final score and allows a Save Score input area
 // Retry button also appears
+
 function endGame() {
 
   quiz.innerHTML = '<p> End</p>';
 
-	 message.innerHTML = `
+  message.innerHTML = `
 	 	<p>You got ${score} of ${questions.length} correct.</p>
      <br><br>
      <form onsubmit="saveScore(event)">
       <input type="text" id = "name" placeholder="Enter Name"></input>
-      <input type="submit" value="Save Score"></input>
+      <input type="submit" value="Save"></input>
 		</form>
   `;
-	secondsLeft = 0;
-	timer.innerHTML = `
-	<button id="retry" onclick="playAgain()">Play Again?</button>
-   `;
 
+
+  secondsLeft = 0;
+  timer.innerHTML = `
+  <button id="retry" onclick="playAgain()">Play Again?</button>
+  `;
 };
 
 function saveScore() {
+  event.preventDefault()
 
-localStorage.setItem('score', score);
+  var inputName = document.getElementById("name").value;
+  scores.push([inputName, score]);
+  // console.log("score on save: ", scores)
+  localStorage.setItem("highScore", JSON.stringify(scores));
 
-  const scoreName = {
-    score: score,
-    name: name.value
+  renderScores();
+};
+
+function renderScores() {
+  // console.log("renderscore");
+  scoresSTR = localStorage.getItem("highScore");
+  scores = JSON.parse(scoresSTR)
+  console.log("scores:", scores, scores.length)
+  for (i = 0; i < scores.length; i++) {
+    // console.log("i:", i)
+    var list = `<li>${scores[i][0]}, ${scores[i][1]}</li>`
+    // console.log(list);
+    $("#list").append(list);
+
+
   }
-  highScores.push(scoreName);
-  console.log(highScores);
 
-}
+};
+
 
 // Function to run the Play Again button, this restarts the quiz
 function playAgain() {
@@ -136,17 +162,21 @@ function playAgain() {
 	currentQuestion = 0;
   score = 0;
   message.innerHTML = ``;
+  // makes the list empty so it can be displayed again from the start when the next game is over
+  $("#list").empty();
 	startQuiz();
 };
 
-/*
- on page load: 
+
+
+/* Pseudo Code
+ on page load:
   show game start screen
   include how to play instructions
   add start quiz button
 
   on start button click:
-    hide game-start div 
+    hide game-start div
     show questions div
     show question
     start timer
